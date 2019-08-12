5310494ce31342a17bdbb22002c4111889ebe485
Michael <michael@unetresgrossebite.com>
2019-08-12 14:13:10 +0200

    fixed issue with nfs installer

--- setup/00_nfs.sh
+++ setup/00_nfs.sh
@@ -7,7 +7,7 @@ build_path=/opt/puppeteer/builds
 if [[ $NFS_BUILD_IP ]] ; then
   if [[ -z `grep /opt/puppeteer/builds /etc/fstab` ]] ; then
     mount $NFS_BUILD_IP:$build_path $build_path
-    echo "$NFS_BUILD_IP:$build_path $build_path nfs defaults,user,auto,noatime,ro 0 0" > /etc/fstab
+    echo "$NFS_BUILD_IP:$build_path $build_path nfs defaults,user,auto,noatime,ro 0 0" >> /etc/fstab
   fi
 else
   echo "(!) Variable NFS_BUILD_IP unset. Puppeteer's build directory won't be mounted"
