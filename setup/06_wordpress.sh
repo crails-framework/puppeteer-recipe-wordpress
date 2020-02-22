644b0f7fc4c9c0db44f16acceb4fb8199edee18c
Michael <michael@unetresgrossebite.com>
2020-02-22 18:50:09 +0100

    * no longer links wp-content to build path * no longer creates admin user (it would require wordpress to have been   set up already)

--- setup/06_wordpress.sh
+++ setup/06_wordpress.sh
@@ -37,27 +37,15 @@ chown -R $APP_USER:`get_http_group_name` "$app_dir"
 chmod -R 775 "$app_dir"
 
 #
-# Link wp-content to Build path
-#
-wp_content_path="$app_dir/wp-content"
-
-if [[ -L "$wp_content_path" && -d "$wp_content_path" ]] ; then
-  unlink "$wp_content_path"
-else
-  rm -Rf "$wp_content_path"
-fi
-
-ln -s "$BUILD_PATH" "$wp_content_path"
-
-#
 # Create admin user, if no user exists (auth data: root;password)
 #
-user_count=`mysql -sN -e "SELECT COUNT(ID) FROM \`$INSTANCE_NAME\`.wp_users"`
-
-if [[ $user_count == "0" ]] ; then
-  encrypted_admin_password='$P$B4sJvG64B5N6Tz81DglGUySJRIc/mf.'
-  mysql -e "INSERT INTO \`$INSTANCE_NAME\`.wp_users VALUES ('', 'root', '$encrypted_admin_password', 'root', 'contact@company.com', '', '', '', 0, 'root')"
-  admin_user_id=`mysql -sN -e "SELECT ID FROM \`$INSTANCE_NAME\`.wp_users ORDER BY ID DESC LIMIT 1"`
-  mysql -e "INSERT INTO \`$INSTANCE_NAME\`.wp_usermeta VALUES(NULL, $admin_user_id, 'wp_capabilities', 'a:1:{s:13:\"administrator\";b:1;}')"
-  mysql -e "INSERT INTO \`$INSTANCE_NAME\`.wp_usermeta VALUES(NULL, $admin_user_id, 'wp_user_level', '10')"
-fi
+# WARN: that's stupid and cannot be done here. Wordpress must be initialized first.
+#user_count=`mysql -sN -e 'SELECT COUNT(ID) FROM \`'$INSTANCE_NAME'\`.wp_users'`
+#
+#if [[ $user_count == "0" ]] ; then
+#  encrypted_admin_password='$P$B4sJvG64B5N6Tz81DglGUySJRIc/mf.'
+#  mysql -e "INSERT INTO \`$INSTANCE_NAME\`.wp_users VALUES ('', 'root', '$encrypted_admin_password', 'root', 'contact@company.com', '', '', '', 0, 'root')"
+#  admin_user_id=`mysql -sN -e "SELECT ID FROM \`$INSTANCE_NAME\`.wp_users ORDER BY ID DESC LIMIT 1"`
+#  mysql -e "INSERT INTO \`$INSTANCE_NAME\`.wp_usermeta VALUES(NULL, $admin_user_id, 'wp_capabilities', 'a:1:{s:13:\"administrator\";b:1;}')"
+#  mysql -e "INSERT INTO \`$INSTANCE_NAME\`.wp_usermeta VALUES(NULL, $admin_user_id, 'wp_user_level', '10')"
+#fi
