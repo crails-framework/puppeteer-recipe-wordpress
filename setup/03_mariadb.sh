243526bd09944b81ba22a58d8ef2e50a203c11ae
Michael <michael@unetresgrossebite.com>
2020-03-01 11:02:55 +0100

    makes sql database name configurable through the APP_SQL_DATABASE variable

--- setup/03_mariadb.sh
+++ setup/03_mariadb.sh
@@ -6,6 +6,10 @@ apt-get install -y mariadb-server
 
 systemctl enable mariadb.service
 
+if [[ -z "$APP_SQL_DATABASE" ]] ; then
+  export APP_SQL_DATABASE="$INSTANCE_NAME"
+fi
+
 user_exists=`mysql -e "SELECT COUNT(User) FROM mysql.user WHERE User='$APP_USER'" | grep 1 || echo "0"`
 
 if [[ $user_exists == 1 ]] ; then
@@ -14,5 +18,5 @@ else
   mysql -e "CREATE USER '$APP_USER'@'localhost' IDENTIFIED BY '$APP_SQL_PASSWORD';"
 fi
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`$INSTANCE_NAME\`"
-mysql -e "GRANT ALL PRIVILEGES ON \`$INSTANCE_NAME\`.* TO '$APP_USER'@'localhost';"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$APP_SQL_DATABASE\`"
+mysql -e "GRANT ALL PRIVILEGES ON \`$APP_SQL_DATABASE\`.* TO '$APP_USER'@'localhost';"
