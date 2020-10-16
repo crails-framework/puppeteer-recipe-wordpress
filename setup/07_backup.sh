c100350f95216f340f425b49bcb7911797ef8f4c
Michael <michael@unetresgrossebite.com>
2020-10-16 16:33:32 +0200

    fixed backup and restore scripts permissions

--- setup/07_backup.sh
+++ setup/07_backup.sh
@@ -4,3 +4,4 @@ source ./variables 2> /dev/null
 
 cat wordpress.backup  > "$APP_PATH/backup.sh"
 cat wordpress.restore > "$APP_PATH/restore.sh"
+chmod +x "$APP_PATH/backup.sh" "$APP_PATH/restore.sh"
