2b941e16cf128f7ccae432ee3e755ceb2e9586bf
Michael <michael@unetresgrossebite.com>
2020-10-23 22:28:39 +0200

    fixed database uninstall not using APP_SQL_DATABASE variable

--- uninstall/02_remove_database.sh
+++ uninstall/02_remove_database.sh
@@ -2,4 +2,8 @@
 
 source ./variables 2> /dev/null
 
-mysql -e "DROP DATABASE $INSTANCE_NAME;"
+if [[ -z "$APP_SQL_DATABASE" ]] ; then
+  export APP_SQL_DATABASE="$INSTANCE_NAME"
+fi
+
+mysql -e "DROP DATABASE $APP_SQL_DATABASE;"
