ead1ec4e0469fa8cc746de85435aee7e923c4218
Michael <michael@unetresgrossebite.com>
2020-10-23 22:27:04 +0200

    adds APP_SQL_USER variable

--- setup/03_mariadb.sh
+++ setup/03_mariadb.sh
@@ -10,13 +10,17 @@ if [[ -z "$APP_SQL_DATABASE" ]] ; then
   export APP_SQL_DATABASE="$INSTANCE_NAME"
 fi
 
-user_exists=`mysql -e "SELECT COUNT(User) FROM mysql.user WHERE User='$APP_USER'" | grep 1 || echo "0"`
+if [[ -z "$APP_SQL_USER" ]] ; then
+  export APP_SQL_USER="$APP_USER"
+fi
+
+user_exists=`mysql -e "SELECT COUNT(User) FROM mysql.user WHERE User='$APP_SQL_USER'" | grep 1 || echo "0"`
 
 if [[ $user_exists == 1 ]] ; then
-  mysql -e "SET PASSWORD FOR '$APP_USER'@'localhost' = PASSWORD('$APP_SQL_PASSWORD');"
+  mysql -e "SET PASSWORD FOR '$APP_SQL_USER'@'localhost' = PASSWORD('$APP_SQL_PASSWORD');"
 else
-  mysql -e "CREATE USER '$APP_USER'@'localhost' IDENTIFIED BY '$APP_SQL_PASSWORD';"
+  mysql -e "CREATE USER '$APP_SQL_USER'@'localhost' IDENTIFIED BY '$APP_SQL_PASSWORD';"
 fi
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`$APP_SQL_DATABASE\`"
-mysql -e "GRANT ALL PRIVILEGES ON \`$APP_SQL_DATABASE\`.* TO '$APP_USER'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON \`$APP_SQL_DATABASE\`.* TO '$APP_SQL_USER'@'localhost';"
